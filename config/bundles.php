15b66dffd8794067eec26ea715d207f0dcc8c029
william <william.del.rosario@iceshop.nl>
2019-09-13 16:32:54 +0200

    Thumbnailing

--- config/bundles.php
+++ config/bundles.php
@@ -18,5 +18,6 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Http\HttplugBundle\HttplugBundle::class => ['all' => true]
+    Http\HttplugBundle\HttplugBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
