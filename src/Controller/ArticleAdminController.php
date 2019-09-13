86f2d3ba3b6cee82646969bc82770b8c46f4e34e
william <william.del.rosario@iceshop.nl>
2019-09-13 07:16:32 +0200

    File naming

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -6,6 +6,7 @@ use App\Entity\Article;
 use App\Form\ArticleFormType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Gedmo\Sluggable\Util\Urlizer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
@@ -77,9 +78,12 @@ class ArticleAdminController extends BaseController
         $uploadedFile = $request->files->get('image');
         $destination = $this->getParameter('kernel.project_dir').'/public/uploads';
 
+        $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+        $newFilename = Urlizer::urlize($originalFilename).'-'.uniqid().'.'.$uploadedFile->guessExtension();
+
         dd($uploadedFile->move(
             $destination,
-            $uploadedFile->getClientOriginalName()
+            $newFilename
         ));
     }
 
