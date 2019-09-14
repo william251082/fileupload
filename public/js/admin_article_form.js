24fb6826772205da3e0a2ff5be6f4be73badf9b8
william <william.del.rosario@iceshop.nl>
2019-09-14 20:09:52 +0200

    js rendering of admin article reference

--- public/js/admin_article_form.js
+++ public/js/admin_article_form.js
@@ -1,7 +1,10 @@
 Dropzone.autoDiscover = false;
 
 $(document).ready(function() {
-    initializeDropzone()
+    var referenceList = new ReferenceList($('.js-reference-list'));
+
+    initializeDropzone(referenceList);
+
     var $locationSelect = $('.js-article-form-location');
     var $specificLocationTarget = $('.js-specific-location-target');
 
@@ -28,7 +31,100 @@ $(document).ready(function() {
     });
 });
 
-function initializeDropzone() {
+// todo - use Webpack Encore so ES6 syntax is transpiled to ES5
+class ReferenceList
+{
+    constructor($element) {
+        this.$element = $element;
+        this.sortable = Sortable.create(this.$element[0], {
+            handle: '.drag-handle',
+            animation: 150,
+            onEnd: () => {
+                $.ajax({
+                    url: this.$element.data('url')+'/reorder',
+                    method: 'POST',
+                    data: JSON.stringify(this.sortable.toArray())
+                });
+            }
+        });
+        this.references = [];
+        this.render();
+
+        this.$element.on('click', '.js-reference-delete', (event) => {
+            this.handleReferenceDelete(event);
+        });
+
+        this.$element.on('blur', '.js-edit-filename', (event) => {
+            this.handleReferenceEditFilename(event);
+        });
+
+        $.ajax({
+            url: this.$element.data('url')
+        }).then(data => {
+            this.references = data;
+            this.render();
+        })
+    }
+
+    addReference(reference) {
+        this.references.push(reference);
+        this.render();
+    }
+
+    handleReferenceDelete(event) {
+        const $li = $(event.currentTarget).closest('.list-group-item');
+        const id = $li.data('id');
+        $li.addClass('disabled');
+
+        $.ajax({
+            url: '/admin/article/references/'+id,
+            method: 'DELETE'
+        }).then(() => {
+            this.references = this.references.filter(reference => {
+                return reference.id !== id;
+            });
+            this.render();
+        });
+    }
+
+    handleReferenceEditFilename(event) {
+        const $li = $(event.currentTarget).closest('.list-group-item');
+        const id = $li.data('id');
+        const reference = this.references.find(reference => {
+            return reference.id === id;
+        });
+        reference.originalFilename = $(event.currentTarget).val();
+
+        $.ajax({
+            url: '/admin/article/references/'+id,
+            method: 'PUT',
+            data: JSON.stringify(reference)
+        });
+    }
+
+    render() {
+        const itemsHtml = this.references.map(reference => {
+            return `
+<li class="list-group-item d-flex justify-content-between align-items-center" data-id="${reference.id}">
+    <span class="drag-handle fa fa-reorder"></span>
+    <input type="text" value="${reference.originalFilename}" class="form-control js-edit-filename" style="width: auto;">
+
+    <span>
+        <a href="/admin/article/references/${reference.id}/download" class="btn btn-link btn-sm"><span class="fa fa-download" style="vertical-align: middle"></span></a>
+        <button class="js-reference-delete btn btn-link btn-sm"><span class="fa fa-trash"></span></button>
+    </span>
+</li>
+`
+        });
+
+        this.$element.html(itemsHtml.join(''));
+    }
+}
+
+/**
+ * @param {ReferenceList} referenceList
+ */
+function initializeDropzone(referenceList) {
     var formElement = document.querySelector('.js-reference-dropzone');
 
     if (!formElement) {
@@ -38,6 +134,9 @@ function initializeDropzone() {
     var dropzone = new Dropzone(formElement, {
         paramName: 'reference',
         init: function () {
+            this.on('success', function (file, data) {
+               referenceList.addReference(data);
+            });
             this.on('error', function (file, data) {
                 if (data.detail) {
                     this.emit('error', file, data.detail);
--- src/Controller/ArticleReferenceAdminController.php
+++ src/Controller/ArticleReferenceAdminController.php
@@ -82,6 +82,22 @@ class ArticleReferenceAdminController extends BaseController
     }
 
     /**
+     * @Route("/admin/article/{id}/references", name="admin_article_list_references", methods={"GET"})
+     * @IsGranted("MANAGE", subject="article")
+     */
+    public function getArticleReferences(Article $article)
+    {
+        return $this->json(
+            $article->getArticleReferences(),
+            200,
+            [],
+            [
+                'groups' => ['main']
+            ]
+        );
+    }
+
+    /**
      * @Route("/admin/article/references/{id}/download", name="admin_article_download_reference", methods={"GET"})
      */
     public function downloadArticleReference(ArticleReference $reference, UploaderHelper $uploaderHelper)
