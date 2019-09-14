5a023c27a1c80ee16d1af5551fa5550b3fbe2953
william <william.del.rosario@iceshop.nl>
2019-09-14 15:12:41 +0200

    Mime type Validation -- mime type and NotBlank validation constraints

--- src/Controller/ArticleReferenceAdminController.php
+++ src/Controller/ArticleReferenceAdminController.php
@@ -12,6 +12,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ArticleReferenceAdminController extends BaseController
 {
@@ -20,11 +24,49 @@ class ArticleReferenceAdminController extends BaseController
      * @IsGranted("MANAGE", subject="article")
      */
     public function uploadArticleReference(
-        Article $article, Request $request, UploaderHelper $uploaderHelper, EntityManagerInterface $manager
+        Article $article,
+        Request $request,
+        UploaderHelper $uploaderHelper,
+        EntityManagerInterface $manager,
+        ValidatorInterface $validator
     )
     {
         /** @var UploadedFile $uploadedFile */
         $uploadedFile = $request->files->get('reference');
+
+        // object that holds errors
+        $violations = $validator->validate(
+            $uploadedFile,
+            [
+                new NotBlank([
+                    'message' => 'Please select a file to upload'
+                ]),
+                new File([
+                    'maxSize' => '5M',
+                    'mimeTypes' => [
+                        'image/*',
+                        'application/pdf',
+                        'application/msword',
+                        'application/vnd.ms-excel',
+                        'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                        'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+                        'text/plain'
+                    ],
+                ])
+            ]
+        );
+
+        if ($violations->count() > 0) {
+            /** @var ConstraintViolation $violation */
+            $violation = $violations[0];
+
+            $this->addFlash('error', $violation->getMessage());
+
+            return $this->redirectToRoute('admin_article_edit', [
+                'id' => $article->getId()
+            ]);
+        }
 
         $filename = $uploaderHelper->uploadArticleReference($uploadedFile);
 
